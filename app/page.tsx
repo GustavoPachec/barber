335201fb95bc40d5a8e8de945fd11fda35016b24
luzig6977@gmail.com <luizg6977@gmail.com>
2024-08-13 22:32:55 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,66 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import { Badge } from "./_components/ui/badge"
+import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
-  return <Button>Click Here</Button>
+  return (
+    <div>
+      {/*HEADER*/}
+      <Header />
+      <div className="p-5">
+        {/*TEXTO*/}
+        <h2 className="text-xl font-bold">Olá, Gustavo!</h2>
+        <p>Quarta-feira, 07 de agosto.</p>
+
+        {/* BUSCA */}
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        {/* IMAGEM */}
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner01.png"
+            fill
+            className="rounded-xl object-cover"
+            alt="Agende nos melhores com FSW Barber"
+          />
+        </div>
+
+        {/* AGENDAMENTO*/}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://www.bing.com/th?id=OIP.ggVEFA2uuXv0uRKdNjdVCwHaHa&w=112&h=106&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" />
+                </Avatar>
+                <p className="text-sm">Barbearia LG</p>
+              </div>
+            </div>
+            {/* DIREITA */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">10</p>
+              <p className="text-sm">10:00</p>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  )
 }
 
 export default Home
