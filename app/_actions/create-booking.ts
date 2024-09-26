33854b4327a3aa077ab82a7a46772f99d7965271
Luiz Gustavo <87072616+GustavoPachec@users.noreply.github.com>
2024-09-26 14:00:09 +0000

    feat: ensure booking can be canceled

--- app/_actions/create-booking.ts
+++ app/_actions/create-booking.ts
@@ -1,5 +1,6 @@
 "use server"
 
+import { revalidatePath } from "next/cache"
 import { db } from "../_lib/prisma"
 
 interface CreateBoakingParams {
@@ -12,4 +13,6 @@ export const createBooking = async (params: CreateBoakingParams) => {
   await db.booking.create({
     data: params,
   })
+  revalidatePath("barbershops/[id]")
+  revalidatePath("/bookings")
 }
