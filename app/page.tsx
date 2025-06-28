6b051ff1fddbb1f812630a4be7820a8362b59b9b
GustavoPachec <gustavo.pacheco.dev@gmail.com>
2025-06-28 13:29:39 -0300

    refactor: replace old quick search implementation with new QuickSearch component

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
 import Header from "./_components/header"
-import { Button } from "./_components/ui/button"
 import Image from "next/image"
 import { db } from "./_lib/prisma"
-import BarbershopItem from "./_components/barbershop-item"
-import { quickSearchOptions } from "./_constants/search"
 import BookingItem from "./_components/booking-item"
 import Search from "./_components/search"
-import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
 import { format } from "date-fns"
 import { ptBR } from "date-fns/locale"
 import { getConfirmedBookings } from "./_data/get-confirmed-booking"
 import { CarouselBarbershopItem } from "./_components/carousel-barbershop-item"
+import QuickSearch from "./_components/quick-search"
 
 const Home = async () => {
   // chamar o banco de dados
@@ -30,7 +27,7 @@ const Home = async () => {
       {/*HEADER*/}
       <Header />
       <div className="mx-auto px-3 py-5 lg:p-5">
-        {/*TEXTO*/}
+        {/*TEXTO BOAS VINDAS*/}
         <h2 className="text-center text-lg font-bold lg:text-xl">
           Olá, {session?.user ? session.user.name : "Bem vindo"}
         </h2>
@@ -47,31 +44,14 @@ const Home = async () => {
         <div className="mt-4 lg:mt-6">
           <Search />
         </div>
+
         {/* BUSCA RÁPIDA */}
-        <div className="mt-4 flex gap-2 overflow-x-auto lg:mt-6 lg:gap-3 [&::-webkit-scrollbar]:hidden">
-          {quickSearchOptions.map((option) => {
-            return (
-              <Button
-                className="min-w-max gap-2"
-                variant="secondary"
-                key={option.title}
-                asChild
-              >
-                <Link href={`/barbershops?service=${option.title}`}>
-                  <Image
-                    src={option.imageURL}
-                    width={16}
-                    height={16}
-                    alt={option.title}
-                  />
-                  <span className="text-xs lg:text-sm">{option.title}</span>
-                </Link>
-              </Button>
-            )
-          })}
+        <div>
+          <QuickSearch />
         </div>
+
         {/* IMAGEM */}
-        <div className="relative mt-4 h-[120px] w-full sm:h-[150px] lg:mt-6">
+        <div className="relative mt-4 h-[120px] w-full lg:mt-6 lg:h-[200px] xl:h-[280px]">
           <Image
             src="/banner01.png"
             fill
@@ -81,12 +61,12 @@ const Home = async () => {
           />
         </div>
 
+        {/* AGENDAMENTOS */}
         {confirmedBookings.length > 0 && (
           <>
             <h2 className="mb-2 mt-4 text-xs font-bold uppercase text-gray-400 lg:mb-3 lg:mt-6">
               Agendamentos
             </h2>
-            {/* AGENDAMENTO*/}
             <div className="flex gap-2 overflow-x-auto lg:gap-3 [&::-webkit-scrollbar]:hidden">
               {confirmedBookings.map((booking) => (
                 <BookingItem key={booking.id} booking={booking} />
